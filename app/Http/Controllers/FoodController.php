10f7c9cdf18ddd8456c81dac3ddfab5f2b7c0db3
Esteban <utti212031@utvco.edu.mx>
2024-06-05 08:41:02 -0600

    Update(FoodController): delete empty functions in foodController

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -27,51 +27,11 @@ class FoodController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function newFood(Request $request)
     {
         $food =  food::create($request->all());
         return response()->json($food, 200);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(food $food)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(food $food)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, food $food)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(food $food)
-    {
-        //
     }
 }
