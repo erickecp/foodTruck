685609511cf1860ec6bf5f568fe04eb10f6f9f3c
password <romero123mar123@gmail.com>
2024-06-05 08:43:46 -0600

    cambios en el html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,6 +31,11 @@
                 </div>
             @endif
 
+            <h1>Commit Desde la compu del Marco</h1>
+            <p>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aspernatur corporis officia minima laboriosam voluptate eligendi asperiores eaque, cumque ab veniam accusamus inventore ratione obcaecati facere ut totam recusandae quibusdam.</span>
+            </p>
+
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
                     <svg viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto bg-gray-100 dark:bg-gray-900">
@@ -126,6 +131,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                 </svg>
                                 Sponsor
+
                             </a>
                         </div>
                     </div>
